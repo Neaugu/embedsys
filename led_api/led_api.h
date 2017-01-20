7b69b206140c6f8b8eb2fcf8ec8b230408d36b73
JulienTomezach <julien.tomezach@ensta-bretagne.org>
2017-01-20 12:32:57 +0100

    Remove useless old folders

--- led_api/led_api.h
+++
@@ -1,4 +0,0 @@
-void dot();
-void dash();
-void write_file(int value);
-void play_morse(char* sentence);
--- led_api/test_led_api.c
+++
@@ -1,8 +0,0 @@
-#include <led_api.h>
-
-int main() {
-  // call a function in another file
-   play_morse("3000211120003");
-
-  return(0);
-}
